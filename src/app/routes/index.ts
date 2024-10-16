2d26e7f67a532cfcc708fbec01f56bd00a31c1f1
natali <NataliDegtyreva@yandex.ru>
2024-10-16 16:55:39 +0300

    feat: add new pages

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,13 @@
 import Login from '../../pages/login'
 import Register from '../../pages/register'
+import Home from '../../pages/home'
+import Page404 from '../../pages/page-404'
+import Page500 from '../../pages/page-500'
 
 export default {
   Login: Login,
-  Register: Register
+  Register: Register,
+  Home: Home,
+  Page404: Page404,
+  Page500: Page500,
 }
