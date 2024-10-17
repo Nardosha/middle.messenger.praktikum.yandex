38cd95da3e7a9cb4ce96bd025bc90f1a39860c11
natali <NataliDegtyreva@yandex.ru>
2024-10-17 17:34:27 +0300

    feat: add profile page and user-profile widget templates

--- src/app/index.ts
+++ src/app/index.ts
@@ -18,6 +18,7 @@ import Pane from '../widgets/pane'
 import Chat from '../widgets/chat'
 import Sidebar from '../widgets/sidebar'
 import ConversationList from '../widgets/conversation-list'
+import UserProfile from '../widgets/user-profile'
 import BurgerButton from '../features/burger-button'
 
 Handlebars.registerPartial('RoundButton', RoundButton)
@@ -35,6 +36,7 @@ Handlebars.registerPartial('Sidebar', Sidebar)
 Handlebars.registerPartial('Pane', Pane)
 Handlebars.registerPartial('Chat', Chat)
 Handlebars.registerPartial('Banner', Banner)
+Handlebars.registerPartial('UserProfile', UserProfile)
 Handlebars.registerPartial('ConversationList', ConversationList)
 
 Handlebars.registerHelper("arr", (...args) => args.slice(0, -1))
@@ -42,7 +44,7 @@ Handlebars.registerHelper("arr", (...args) => args.slice(0, -1))
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('app')
-  const template = Handlebars.compile(Pages.Home)
-  const result = template({ConversationList})
+  const template = Handlebars.compile(Pages.Profile)
+  const result = template({})
   root.innerHTML = result
 })
